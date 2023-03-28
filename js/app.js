592ea6c712d931f93124db0a1bad996db1ac5b84
Arafat817 <gr1arafat@gmail.com>
2023-03-29 03:09:14 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -38,7 +38,9 @@ const displayPhones = (phones,dataLimit) =>{
       <div class="card-body">
         <h5 class="card-title">${phone.phone_name}</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
+        <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal" >Show details</button>
+              
+        </div>
      </div>
      
     `;
@@ -58,6 +60,12 @@ document.getElementById('btn-search').addEventListener('click', function(){
   //start loader
   processSearch(10);
 })
+//search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function(e){
+   if (e.key === 'Enter'){
+    processSearch(10);
+   }
+})
 
 const toggleSpinner = isLoding => {
   const loaderSection = document. getElementById('loader');
@@ -73,4 +81,22 @@ document.getElementById('btn-show-all').addEventListener('click', function(){
   processSearch();
 })
 
-// loadPhones();
+const loadPhoneDetails = async id => {
+  const url= `https://openapi.programming-hero.com/api/phone/${id}`
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone =>{
+  const modalTital = document.getElementById('phoneDetailModalLabel');
+  modalTital.innerText = phone.name;
+  const phoneDetails = document.getElementById('phone-details');
+  phoneDetails.innerText=`
+  <p> Release Date: ${phone.releaseDate ? phone.releaseDate : 'No release date found'} </p>
+  <p> Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No storage info'} </p>
+  <p> Others: ${phone.others ? phone.others.Bluetooth : 'No bluetooth information'} </p>
+  `
+}
+
+ loadPhones('apple');
